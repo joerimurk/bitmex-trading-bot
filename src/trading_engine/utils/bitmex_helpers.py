e2582817999d533a03a40f56510bbfe0984249bb
Joeri Murk <joeri.murk@indatables.com>
2024-04-30 16:59:01 +0200

    fix: websocket open orders unstable so replaced by price websocket

--- src/trading_engine/utils/bitmex_helpers.py
+++ src/trading_engine/utils/bitmex_helpers.py
@@ -1,14 +1,9 @@
-import hashlib
-import hmac
-import json
-import os
 import time
-import urllib
 
 import numpy as np
-from websocket import create_connection
-
-from src.trading_engine.utils.constants import BITMEX_URL, ENDPOINT, VERB
+import websocket
+from loguru import logger
+import json
 
 
 def limit_order(client, symbol, order_quantity, price):
@@ -30,7 +25,7 @@ def cancel_open_orders(client):
     _ = client.Order.Order_cancelAll().result()
 
 
-def get_open_positions(client, symbol):
+def get_open_positions(client):
     """Bitmex get all open orders"""
     return client.Position.Position_get().result()[0]
 
@@ -43,65 +38,79 @@ def calculate_order_price(price, margin, positive):
         return np.round(price - (margin * price), 1)
 
 
-def websocket_open_orders(timeout=None):
-    """Bitmex websocket for changes in open orders"""
-    expires = int(time.time()) + 5
-    signature = bitmex_signature(
-        os.getenv("BITMEX_API_SECRET"), VERB, ENDPOINT, expires
-    )
+class WebsocketPrice:
+    def __init__(
+        self,
+        client,
+        symbol: str = "ETHUSD",
+        buy_price: float = None,
+        sell_price: float = None,
+        timeout: float = None,
+    ):
+        """Websocket for current price"""
 
-    # Initial connection - BitMEX sends a welcome message.
-    ws = create_connection(BITMEX_URL + ENDPOINT, timeout=10)
-    _ = ws.recv()
+        self.client = client
+        self.symbol = symbol
+        self.buy_price = buy_price
+        self.sell_price = sell_price
+        self.timeout = timeout
 
-    # Send API Key with signed message.
-    request = {
-        "op": "authKeyExpires",
-        "args": [os.getenv("BITMEX_API_KEY"), expires, signature],
-    }
-    ws.send(json.dumps(request))
-    _ = ws.recv()
+        if not self.buy_price:
+            self.buy_price = -np.inf
+        if not self.sell_price:
+            self.sell_price = np.inf
+        if not self.timeout:
+            self.timeout = np.inf
 
-    # Send a request that requires authorization.
-    request = {"op": "subscribe", "args": "order"}
-    ws.send(json.dumps(request))
-    _ = ws.recv()
+        self.buy_price_reached = False
+        self.sell_price_reached = False
+        self.timeout_reached = False
 
-    # CHECK IF ONLY A SINGLE ORDER OPEN
-    _ = ws.recv()
-    # data = json.loads(result)["data"]
-    # print(f"Only one order open: {len(data)==1}")
-    # print(f"Open order: {data[0]}")
+        self.start_time = time.time()
 
-    # Wait for socket to respond
-    try:
-        _ = ws.recv()
-        bought_successfully = True
-    except:
-        bought_successfully = False
+    def on_message(self, ws, message):
+        """Function called everytime the price changes"""
+        current_price = self.get_price(message)
+        if current_price:
+            if current_price <= self.buy_price:
+                self.buy_price_reached = True
+                ws.close()
+            elif current_price >= self.sell_price:
+                self.sell_price_reached = True
+                ws.close()
 
-    ws.close()
+        if (time.time() - self.start_time) > self.timeout:
+            self.timeout_reached = True
+            ws.close()
 
-    return bought_successfully
+    def on_error(self, ws, error):
+        """Function called after an error"""
+        logger.error(error)
 
+    def on_close(self, ws, a, b):
+        """Function called when closing websocket"""
+        logger.info("### closed ###")
 
-def bitmex_signature(apiSecret, verb, url, nonce, postdict=None):
-    """Given an API Secret key and data, create a BitMEX-compatible signature."""
-    data = ""
-    if postdict:
-        # separators remove spaces from json
-        # BitMEX expects signatures from JSON built without spaces
-        data = json.dumps(postdict, separators=(",", ":"))
-    parsedURL = urllib.parse.urlparse(url)
-    path = parsedURL.path
-    if parsedURL.query:
-        path = path + "?" + parsedURL.query
-    # print("Computing HMAC: %s" % verb + path + str(nonce) + data)
-    message = (verb + path + str(nonce) + data).encode("utf-8")
-    # print("Signing: %s" % str(message))
+    @staticmethod
+    def get_price(message):
+        """Get price from websocket output"""
+        # load message from websocket
+        message = json.loads(message)
 
-    signature = hmac.new(
-        apiSecret.encode("utf-8"), message, digestmod=hashlib.sha256
-    ).hexdigest()
-    # print("Signature: %s" % signature)
-    return signature
+        # find price in message
+        if "data" in message.keys():
+            if "lastPrice" in message["data"][0].keys():
+                return message["data"][0]["lastPrice"]
+        return None
+
+    def start_websocket(self):
+        websocket.enableTrace(False)
+        ws = websocket.WebSocketApp(
+            f"wss://ws.bitmex.com/realtime?subscribe=instrument:{self.symbol}",
+            on_message=self.on_message,
+            on_close=self.on_close,
+            on_error=self.on_error,
+        )
+        ws.run_forever()
+
+        return self.buy_price_reached, self.sell_price_reached, self.timeout_reached
