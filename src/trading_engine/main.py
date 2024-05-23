642aea879771874ce479588f614aca798ba8bc7a
Joeri Murk <joeri.murk@indatables.com>
2024-05-23 15:36:35 +0200

    feat: initiate database

--- src/trading_engine/main.py
+++ src/trading_engine/main.py
@@ -1,14 +1,12 @@
 import os
 
 import bitmex
-from dotenv import load_dotenv
-
-from src.trading_engine.utils.constants import (BUY_EXTRA_MARGIN,
-                                                ORDER_QUANTITY, PROFIT_MARGIN,
-                                                STABILITY, SYMBOL, TIMEOUT)
-from src.trading_engine.utils.trading_algorithm import TradingAlgorthm
-
-load_dotenv()
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from utils.constants import (BUY_EXTRA_MARGIN, ORDER_QUANTITY, PROFIT_MARGIN,
+                             STABILITY, SYMBOL, TIMEOUT)
+from utils.database_orm import Base
+from utils.trading_algorithm import TradingAlgorthm
 
 BITMEX_CLIENT = bitmex.bitmex(
     api_key=os.getenv("BITMEX_API_KEY"),
@@ -17,6 +15,15 @@ BITMEX_CLIENT = bitmex.bitmex(
 )
 
 if __name__ == "__main__":
+
+    # create database if its not there yet
+    engine = create_engine("sqlite:///app/trades.db")
+    Base.metadata.create_all(engine)
+
+    # database session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
     TradingAlgorthm(
         client=BITMEX_CLIENT,
         symbol=SYMBOL,
@@ -25,4 +32,5 @@ if __name__ == "__main__":
         buy_extra_margin=BUY_EXTRA_MARGIN,
         order_size=ORDER_QUANTITY,
         timeout=TIMEOUT,
+        session=session,
     ).run()
